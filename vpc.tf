aaa14d7922415d2826321c94b7fe188a0d3fb418
Asik Rasool <rasoolasik@gmail.com>
2024-01-24 00:34:40 +0400

    Creating vpc and ec2 instances

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+
+module "vpc" {
+  source = "./tfmodules/vpc"
+
+  env             = "cloudbees"
+  project         = "task1"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  private_subnet_tags = {
+    "terraform"   = "true"
+    "project"     = "task1"
+    "environment" = "cloudbees"
+  }
+
+  public_subnet_tags = {
+    "terraform"   = "true"
+    "project"     = "task1"
+    "environment" = "cloudbees"
+  }
+}
+
